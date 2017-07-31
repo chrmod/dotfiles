23f060a8f79f1bddefe3f1042fed34736ea9dbc7
Krzysztof Jan Modras <chrmod@chrmod.net>
2017-07-31 20:22:43 +0200

    Bash: android paths

--- .config/bash/android.sh
+++ .config/bash/android.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 if [ -d "$HOME/Android/android-studio/bin" ] ; then
-    PATH="$HOME/Android/android-studio/bin:$PATH"
+    export PATH="$HOME/Android/android-studio/bin:$PATH"
 fi
 
 if [ -d "$HOME/Android/Sdk" ] ; then
-    PATH="$HOME/Android/Sdk/emulator/:$HOME/Android/Sdk/platform-tools:$PATH"
-    ANDROID_HOME="$HOME/Android/Sdk"
+    export PATH="$HOME/Android/Sdk/emulator/:$HOME/Android/Sdk/platform-tools:$PATH"
+    export ANDROID_HOME="$HOME/Android/Sdk"
 fi
 
 if [ -d "$HOME/Android/genymotion" ] ; then
-    PATH="$HOME/Android/genymotion:$PATH"
+    export PATH="$HOME/Android/genymotion:$PATH"
 fi
