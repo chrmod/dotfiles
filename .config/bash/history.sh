ef0150d12b43fbc85938c5ba8bbed8ab3a4b9178
Krzysztof Jan Modras <chrmod@chrmod.net>
2017-12-27 21:39:00 +0100

    bash: updating history after every command

--- .config/bash/history.sh
+++ .config/bash/history.sh
@@ -7,3 +7,6 @@ HISTFILESIZE=
 
 # When the shell exits, append to the history file instead of overwriting it
 shopt -s histappend
+
+# Save and reload the history after each command finishes
+export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
