5e33b6d38e37a9b288a7a5bddc66e46295eb8227
Krzysztof Jan Modras <chrmod@chrmod.net>
2018-03-24 21:48:18 +0100

    bash: android env variables

--- .config/bash/android.sh
+++ .config/bash/android.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
+
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
+
 if [ -d "$HOME/Android/android-studio/bin" ] ; then
     export PATH="$HOME/Android/android-studio/bin:$PATH"
 fi
 
 if [ -d "$HOME/Android/Sdk" ] ; then
+    export PATH="$HOME/Android/Sdk/platform-tools:$PATH"
+    export PATH="$HOME/Android/Sdk/tools:$PATH"
+    export PATH="$HOME/Android/Sdk/tools/bin:$PATH"
     export PATH="$HOME/Android/Sdk/emulator/:$PATH"
-    export PATH=":$HOME/Android/Sdk/platform-tools:$PATH"
-    export PATH=":$HOME/Android/Sdk/tools:$PATH"
-    export PATH=":$HOME/Android/Sdk/tools/bin:$PATH"
     export ANDROID_HOME="$HOME/Android/Sdk"
+    export ANDROID_SDK="$ANDROID_HOME"
+    export ANDROID_NDK="$HOME/Android/Sdk/ndk-bundle"
 fi
 
 if [ -d "$HOME/Android/genymotion" ] ; then
     export PATH="$HOME/Android/genymotion:$PATH"
 fi
+
