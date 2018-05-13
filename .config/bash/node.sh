0d3a4386d6a76f0333313f705050b8fa162e8a1b
Krzysztof Jan Modras <chrmod@chrmod.net>
2018-05-13 20:36:33 +0200

    node bash completion

--- .config/bash/node.sh
+++ .config/bash/node.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
+export NVM_DIR="$HOME/.nvm"
+
 [[ -s $HOME/.nvm/nvm.sh ]] && . $HOME/.nvm/nvm.sh # This loads NVM
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # .npmrc credentials
 # https://github.com/npm/npm/issues/8223#issuecomment-103748375
