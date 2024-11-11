9857aaac603492f269f6d0077aafb36e0605b448
Krzysztof Modras <chrmod@chrmod.net>
2024-11-11 13:00:38 +0100

    wezterm + nushel: gruvbox theme

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,8 +1,9 @@
 local wezterm = require 'wezterm'
 local act = wezterm.action
 local config = wezterm.config_builder()
+local gruvbox = require('colors.gruvbox')
 
-config.color_scheme = 'Catppuccin Macchiato'
+config.colors = gruvbox
 
 config.font = wezterm.font 'JetBrains Mono'
 config.font_size = 15.0
